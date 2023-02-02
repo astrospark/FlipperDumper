ad1a3845684a53a794104e6103392a5e17233a1b
Riley McArdle <riley@mcardle.org>
2023-02-01 21:00:34 -0600

    Initial version.

---
+++ FlipperDumper/FlipperDumper.cs
@@ -0,0 +1,42 @@
+﻿using Com.Lego.Flipper.Windows.Content.Providers;
+using Microsoft.Extensions.FileProviders;
+
+var destinationFolderRoot = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "FlipperDumper");
+var contentProvider = new EmbeddedResourcesPrepackedContentProvider();
+
+DumpDirectory("variant");
+
+void DumpDirectory(string? directoryPath)
+{
+    if (string.IsNullOrWhiteSpace(directoryPath)) return;
+
+    var directoryContents = contentProvider.GetDirectoryContents(directoryPath + "/");
+    if (directoryContents == null) return;
+
+    foreach (var fileInfo in directoryContents)
+    {
+        if (string.IsNullOrWhiteSpace(fileInfo.PhysicalPath)) continue;
+
+        Console.WriteLine(fileInfo.PhysicalPath);
+        if (fileInfo.IsDirectory)
+        {
+            Directory.CreateDirectory(Path.Combine(destinationFolderRoot, fileInfo.PhysicalPath));
+            DumpDirectory(fileInfo.PhysicalPath);
+        }
+        else
+        {
+            DumpFile(fileInfo);
+        }
+    }
+}
+
+void DumpFile(IFileInfo? fileInfo)
+{
+    if (fileInfo == null || string.IsNullOrWhiteSpace(fileInfo.PhysicalPath)) return;
+        
+    var sourceStream = fileInfo.CreateReadStream();
+    var destinationStream = File.Create(Path.Combine(destinationFolderRoot, fileInfo.PhysicalPath));
+
+    sourceStream.CopyTo(destinationStream);
+    destinationStream.Close();
+}
---
+++ README.md
@@ -0,0 +1,10 @@
+# FlipperDumper
+
+FlipperDumper extracts the resource files embedded in `LEGO Education SPIKE.dll`. The files are placed in the `FlipperDumper` folder on the desktop. There are over 10,000 files totaling nearly 1.2GB.
+
+This software has only been tested with version 2.0.8 of the SPIKE software but it will probably work with other versions as well.
+
+Flipper appears to be the internal code name for SPIKE, hence the name FlipperDumper.
+
+LEGO and SPIKE are trademarks of the LEGO Group.
+This software is not authorized or endorsed by the LEGO Group.
